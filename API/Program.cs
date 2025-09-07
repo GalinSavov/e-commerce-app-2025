ead79be0000875d72c95b65928cbcebfdfacb600
GalinSavov <galsavov@abv.bg>
2025-09-07 14:44:56 +0300

    Added CORS

--- API/Program.cs
+++ API/Program.cs
@@ -13,11 +13,13 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>(); // this service only exists for the lifetime of a HTTP request
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","htpps://localhost:4200"));
 app.MapControllers();
 try
 {
