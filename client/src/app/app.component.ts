5e0e1f091150f43106d31987bab1893105a8f9d7
GalinSavov <galsavov@abv.bg>
2025-09-09 17:02:20 +0300

    Added a Header component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  protected readonly title = signal('Whammy Store');
+  protected readonly title = signal('Pinch Harmonics');
 }
