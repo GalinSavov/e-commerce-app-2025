31051ae9e7cb8d75f93a8f1abcd1c4ae5ec9a6d6
GalinSavov <galsavov@abv.bg>
2025-10-20 15:42:34 +0300

    Created a cart service

---
+++ Core/Interfaces/ICartService.cs
@@ -0,0 +1,11 @@
+using System;
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface ICartService
+{
+    Task<ShoppingCart?> GetCartAsync(string key);
+    Task<ShoppingCart?> SetCartAsync(ShoppingCart shoppingCart);
+    Task<bool> DeleteCartAsync(string key);
+}
