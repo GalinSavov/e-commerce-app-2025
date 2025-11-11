8c89416a08717ec007cd4a32774fc95614d87d8b
GalinSavov <galsavov@abv.bg>
2025-11-11 11:39:36 +0200

    Fully implemented stripe into the application

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -15,10 +15,12 @@ import { CheckoutDeliveryComponent } from "./checkout-delivery/checkout-delivery
 import { CheckoutReviewComponent } from "./checkout-review/checkout-review.component";
 import { CartService } from '../../core/services/cart.service';
 import { CurrencyPipe, JsonPipe } from '@angular/common';
+import{MatProgressSpinnerModule} from '@angular/material/progress-spinner'
 
 @Component({
   selector: 'app-checkout',
-  imports: [OrderSummaryComponent, MatStepperModule, RouterLink, MatButton, RouterLink, MatCheckboxModule, CheckoutDeliveryComponent, CheckoutReviewComponent,CurrencyPipe,JsonPipe],
+  imports: [OrderSummaryComponent, MatStepperModule, RouterLink, MatButton, RouterLink, MatCheckboxModule, 
+    CheckoutDeliveryComponent, CheckoutReviewComponent,CurrencyPipe,JsonPipe,MatProgressSpinnerModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
@@ -34,6 +36,7 @@ export class CheckoutComponent implements OnInit,OnDestroy {
   protected cartService = inject(CartService);
   protected completionStatus = signal<{address:boolean,card:boolean,delivery:boolean}>({address:false,card:false,delivery:false});
   protected confirmationToken?:ConfirmationToken;
+  loading = false;
 
   async ngOnInit() {
     try {
@@ -95,6 +98,7 @@ export class CheckoutComponent implements OnInit,OnDestroy {
     }
   }
   async confirmPayment(stepper:MatStepper){
+    this.loading = true;
     try {
       if(this.confirmationToken){
         const result = await this.stripeService.confirmPayment(this.confirmationToken);
@@ -109,6 +113,8 @@ export class CheckoutComponent implements OnInit,OnDestroy {
     } catch (error:any) {
       this.snackbarService.error(error.message || 'Something went wrong');
       stepper.previous();
+    } finally{
+      this.loading = false;
     }
   }
   private async getAddressFromStripeAddress():Promise<Address | null> {
