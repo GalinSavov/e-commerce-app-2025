d9521f7d6e4f11281b9a82e2a37277f2bd093e4f
GalinSavov <galsavov@abv.bg>
2025-11-12 15:11:42 +0200

    Added frontend order components and service

---
+++ client/src/app/core/features/order.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Order, OrderToCreate } from '../../shared/models/order';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  baseUrl = environment.apiURL;
+  private http = inject(HttpClient);
+  createOrder(orderToCreate:OrderToCreate){
+    return this.http.post<Order>(this.baseUrl + 'orders',orderToCreate);
+  }
+  getOrders(){
+    return this.http.get<Order[]>(this.baseUrl+'orders');
+  }
+  getOrder(id:number){
+    return this.http.get<Order>(this.baseUrl+'orders/' + id);
+  }
+}
---
+++ client/src/app/shared/models/order.ts
@@ -0,0 +1,44 @@
+export type Order = {
+  id: number
+  orderDate: string
+  buyerEmail: string
+  shippingAddress: ShippingAddress
+  deliveryMethod: string
+  shippingPrice: number
+  paymentSummary: PaymentSummary
+  orderItems: OrderItem[]
+  subTotal: number
+  orderStatus: string
+  paymentIntentId: string
+}
+
+export type ShippingAddress = {
+  name: string
+  line1: string
+  line2?: string
+  city: string
+  state: string
+  postalCode: string
+  country: string
+}
+
+export type PaymentSummary = {
+  last4: number
+  brand: string
+  expMonth: number
+  expYear: number
+}
+
+export type OrderItem = {
+  productId: number
+  productName: string
+  pictureURL: string
+  price: number
+  quantity: number
+}
+export type OrderToCreate = {
+    cartId:string,
+    deliveryMethodId:number,
+    shippingAddress:ShippingAddress,
+    paymentSummary:PaymentSummary
+}
