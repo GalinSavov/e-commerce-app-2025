1cd5e2c696f83c722be46da7ecc41e4eba5fedea
GalinSavov <galsavov@abv.bg>
2026-01-09 16:16:22 +0200

    updated the stripe public key for production

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production:true,
     apiURL: 'api/',
     hubURL:'hub/notifications',
-    stripePublicKey: 'pk_test_51SPjGlP1GaKKMg8E2cy9LXrDeWedsfxbotyIAJzaxpuT6qcmAyQsAvbGBgmYyPv76NNqemuGrQxHb7c0ZPAqaqr000O4W2pf5y'
+    stripePublicKey: 'pk_test_51SnfHzDt6fQdQiULGElfxUeY0K2kZOSQgjydFvwyMs0KDmLRrK6i61Xxm30NA0bKBODaDkt3w5ORVNgVhQkKeLwn00yYwF23Zk'
 };
