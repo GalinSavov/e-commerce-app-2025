152f0d8b3ae382e8359651eae6d9e4b103d5825d
GalinSavov <galsavov@abv.bg>
2026-01-14 15:09:22 +0200

    Updated environment imports in services

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,5 +1,5 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 import { createNewCart, ShoppingCart } from '../../shared/models/shoppingCart';
 import { HttpClient } from '@angular/common/http';
 import { CartItem } from '../../shared/models/cartItem';
