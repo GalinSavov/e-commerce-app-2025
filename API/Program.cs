1402020069328f3f4e6eadf025e2dd0757ec9254
GalinSavov <galsavov@abv.bg>
2026-01-15 15:43:45 +0200

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -61,13 +61,15 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 // CORS must be before authentication & endpoints
-app.UseCors(policy => policy
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(policy => policy
     .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
     .WithOrigins("http://localhost:4200", "https://localhost:4200")
 );
-
+}
 // These are required for cookie auth to work
 app.UseAuthentication();
 app.UseAuthorization();
