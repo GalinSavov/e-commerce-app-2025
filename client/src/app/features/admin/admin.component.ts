9cf038a80e8dcc9c2a3a43ae3b9c6592c85771f2
GalinSavov <galsavov@abv.bg>
2026-02-23 14:32:55 +0200

    Implemented the admin component and template logic

--- client/src/app/features/admin/admin.component.ts
+++ client/src/app/features/admin/admin.component.ts
@@ -1,11 +1,83 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { Order } from '../../shared/models/order';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { AdminService } from '../../core/services/admin.service';
+import { OrderParams } from '../../shared/models/orderParams';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterLink } from '@angular/router';
+import { DialogService } from '../../core/services/dialog.service';
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    DatePipe,
+    CurrencyPipe,
+    MatTooltipModule,
+    MatTabsModule,
+    RouterLink
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'buyerEmail', 'orderDate','total', 'orderStatus','action'];
+  dataSource = new MatTableDataSource<Order>([]);
+  protected adminService = inject(AdminService);
+  protected dialogService = inject(DialogService);
+  orderParams = new OrderParams();
+  totalItems = 0;
+  statusOptions = ['All','Pending','PaymentReceived','PaymentMismatch','Refunded','Pending'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+    ngOnInit(): void {
+    this.loadOrders();
+  }
+
+  loadOrders(){
+    this.adminService.getOrders(this.orderParams).subscribe({
+      next: response =>{
+        if(response.data){
+          this.dataSource.data = response.data;
+          this.totalItems = response.count;
+        }
+      }
+    })
+  }
+  onPageChange(event:PageEvent){
+    this.orderParams.pageNumber = event.pageIndex + 1;
+    this.orderParams.pageSize = event.pageSize;
+    this.loadOrders();
+  }
+  onFilterSelect(event:MatSelectChange){
+    this.orderParams.filter = event.value;
+    this.orderParams.pageNumber = 1;
+    this.loadOrders();
+  }
+  async openConfirmDialog(id:number){
+    const confirmed = await this.dialogService.confirm('Confirm Refund','Are you sure you want to refund this order?');
+    if(confirmed){
+      this.refundOrder(id);
+    }
+  }
+  refundOrder(id:number){
+    this.adminService.refundOrder(id).subscribe({
+      next: order => {
+        this.dataSource.data = this.dataSource.data.map(o => o.id === id ? order : o);
+      }
+    })
+  }
 
 }
--- client/src/app/features/orders/order-detailed/order-detailed.component.ts
+++ client/src/app/features/orders/order-detailed/order-detailed.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { OrderService } from '../../../core/features/order.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Order } from '../../../shared/models/order';
 import { MatCardModule } from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { AddressPipe } from '../../../shared/pipes/address-pipe';
 import { PaymentPipe } from '../../../shared/pipes/payment-pipe';
+import { AccountService } from '../../../core/services/account.service';
+import { AdminService } from '../../../core/services/admin.service';
 
 @Component({
   selector: 'app-order-detailed',
@@ -20,12 +22,20 @@ export class OrderDetailedComponent implements OnInit {
   }
   protected orderService = inject(OrderService);
   protected activatedRoute = inject(ActivatedRoute);
+  protected accountService = inject(AccountService);
+  protected adminService = inject(AdminService);
+  private router = inject(Router);
   order?:Order;
+  buttonText = this.accountService.isAdmin() ? 'Return To Admin' : 'Return To Orders';
   loadOrder(){
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if(!id) return;
-    this.orderService.getOrder(+id).subscribe({
+    const loadOrderData = this.accountService.isAdmin() ? this.adminService.getOrder(+id) : this.orderService.getOrder(+id);
+    loadOrderData.subscribe({
       next:order => this.order = order
     })
+  }
+  onReturnClick(){
+    this.accountService.isAdmin() ? this.router.navigateByUrl('/admin') : this.router.navigateByUrl('/orders');
   }
 }
